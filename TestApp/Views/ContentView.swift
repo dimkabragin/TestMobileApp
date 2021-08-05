7577c7b03426cb22017b001935e0b9c3059fb550
Dimka Bragin <dimkabragin@gmail.com>
2021-08-05 10:12:58 +0700

    Сделал главную форму покрасивее в плане читаемости

--- TestApp/Views/ContentView.swift
+++ TestApp/Views/ContentView.swift
@@ -8,16 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
     @State var phone: String = ""
     @State var code: String = ""
-    
-
     
     @State var isCodeFieldShowing = false
     @State var isButtonDisabled = false
     @State var isFalsePhoneState = false
     @State var isFalseCodeState = false
+    
+    //Состояние логина храним в UserDefaults
     @State var navigated = UserDefaults.standard.bool(forKey: "isLogin")
     
     var body: some View {
@@ -26,51 +25,13 @@ struct ContentView: View {
                 CustomTextField(label: "Введите номер телефона", isCorrect: $isFalsePhoneState, text: $phone, isSecure: false)
                 
                 if isCodeFieldShowing {
-                    
                     TimerLabel(timerValue: 60)
                     
                     CustomTextField(label: "Введите код из смс", isCorrect: $isFalseCodeState, text: $code, isSecure: true)
                 }
                 
-                // Тут, конечно, знатный говнокод, если останется время лучше переделать это чтобы каждый контрол отвечал за свой контент, тогда не потребуется тысяча вложенных if. Но за неимением времени пока оставлю так. Если будет время - поправлю чтобы было повкуснее
-                
                 Button(action: {
-                    isButtonDisabled.toggle()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        if (isCodeFieldShowing == false) {
-                            if (phone == "+79998887766") {
-                                isCodeFieldShowing.toggle()
-                                isFalsePhoneState = false
-                            }
-                            else {
-                                isFalsePhoneState = true
-                            }
-                        }
-                        else {
-                            if (phone == "+79998887766") {
-                                if (code == "1111") {
-                                    self.navigated = true
-                                    UserDefaults.standard.set(true, forKey: "isLogin")
-                                    
-                                    //скидываем состояние к заводским
-                                    code = ""
-                                    phone = ""
-                                    isCodeFieldShowing = false
-                                    isFalseCodeState = false
-                                }
-                                else {
-                                    isFalseCodeState = true;
-                                }
-                            }
-                            else {
-                                code = ""
-                                isCodeFieldShowing = false
-                                isFalsePhoneState = true
-                            }
-                        }
-                        isButtonDisabled.toggle()
-                    }
-                    
+                    StateManager()
                 }) {
                     Text(isCodeFieldShowing ?  "Войти" : "Запросить СМС")
                         .padding()
@@ -80,10 +41,52 @@ struct ContentView: View {
                 .cornerRadius(10)
                 .disabled(isButtonDisabled)
                 
+                // Выполняем переход к изображениям
                 NavigationLink(destination: PicturesListView(), isActive: $navigated) {
                     EmptyView()
                 }
             }
+        }
+    }
+    
+    
+    // Тут, конечно, знатный говнокод, если останется время лучше переделать это чтобы каждый контрол отвечал за свой контент, тогда не потребуется тысяча вложенных if. Но за неимением времени пока оставлю так. Если будет время - поправлю чтобы было повкуснее
+    
+    func StateManager() {
+        isButtonDisabled.toggle()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            if (isCodeFieldShowing == false) {
+                if (phone == "+79998887766") {
+                    isCodeFieldShowing.toggle()
+                    isFalsePhoneState = false
+                }
+                else {
+                    isFalsePhoneState = true
+                }
+            }
+            else {
+                if (phone == "+79998887766") {
+                    if (code == "1111") {
+                        self.navigated = true
+                        UserDefaults.standard.set(true, forKey: "isLogin")
+                        
+                        //скидываем состояние к заводским
+                        code = ""
+                        phone = ""
+                        isCodeFieldShowing = false
+                        isFalseCodeState = false
+                    }
+                    else {
+                        isFalseCodeState = true;
+                    }
+                }
+                else {
+                    code = ""
+                    isCodeFieldShowing = false
+                    isFalsePhoneState = true
+                }
+            }
+            isButtonDisabled.toggle()
         }
     }
 }
